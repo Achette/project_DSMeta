987683f9e9849ef73064e4b8112c48fabeca1929
Achette <i.achette@live.com>
2022-07-15 11:41:41 -0300

    Feat: added toastity

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { Header, SalesCard } from "./components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
-
+  
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/notificationButton/NotificationButton.tsx
+++ frontend/src/components/notificationButton/NotificationButton.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.png";
 import { BASE_URL } from "../../utils/Request";
 import "./NotificationButton.css";
@@ -11,7 +12,7 @@ export const NotificationButton = ({ id }: TNotificationButtonProps) => {
   const handleClickSubmit = (id: number) => {
     axios
       .get(`${BASE_URL}/sales/${id}/notification`)
-      .then((res) => console.log("Sucesso no envio"));
+      .then((res) => toast.info("SMS enviado com sucesso!"));
   };
 
   return (
